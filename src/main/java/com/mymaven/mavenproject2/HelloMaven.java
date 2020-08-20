caf65a922b34d5e9c1a07c69e1ce85245f052562
latif.sunstars@gmail.com <latif.sunstars@gmail.com>
2020-08-20 11:40:02 +0530

    1st commit

---
+++ src/main/java/com/mymaven/mavenproject2/HelloMaven.java
@@ -0,0 +1,13 @@
+package com.mymaven.mavenproject2;
+
+public class HelloMaven {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Maven Demo Project");
+        int i, n=10;
+        for(i=0;i< n;i++)
+        {
+            System.out.println(i);
+        }
+        }
+}
